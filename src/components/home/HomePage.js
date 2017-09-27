7c2e05a7c4f56f825911a317857b54eb34449abd
maimoonaiqbal54 <32327323+maimoonaiqbal54@users.noreply.github.com>
2017-09-27 21:02:14 +0500

    Added basic layout code. Set up Routes

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component
+{
+render()
+{
+  return (
+    <div className="jumbotron">
+      <h1>PluralSights Administration</h1>
+      <p>React, Redux and React Router in ES6 for ultra-responsive web-apps</p>
+      <Link to="about" ClassName="btn btn-primary btn-lg">Learn More</Link>
+    </div>
+  );
+}
+}
+
+export default HomePage;
