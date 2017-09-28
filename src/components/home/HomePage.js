c60f3b65fef8b4037d5a9ea93dcbb12f125b8fe8
maimoonaiqbal54 <32327323+maimoonaiqbal54@users.noreply.github.com>
2017-09-28 16:54:58 +0500

    Redux Store setup files.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class HomePage extends React.Component
-{
-render()
-{
-  return (
-    <div className="jumbotron">
-      <h1>PluralSights Administration</h1>
-      <p>React, Redux and React Router in ES6 for ultra-responsive web-apps</p>
-      <Link to="about" ClassName="btn btn-primary btn-lg">Learn More</Link>
-    </div>
-  );
-}
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>PluralSights Administration</h1>
+        <p>React, Redux and React Router in ES6 for ultra-responsive web-apps</p>
+        <Link to="about" ClassName="btn btn-primary btn-lg">Learn More</Link>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,6 @@ export default (
     <IndexRoute component={HomePage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="courses" component={CoursesPage}/>
-
   </Route>
 );
 
